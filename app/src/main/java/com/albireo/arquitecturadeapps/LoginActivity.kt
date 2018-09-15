28dd8a95fdd657b3cc05b316da0f7c7b023d450e
Diego Cardenas <diegoefraincardenas@gmail.com>
2018-09-15 13:04:22 -0500

    adding view components

--- app/src/main/java/com/albireo/arquitecturadeapps/LoginActivity.kt
+++ app/src/main/java/com/albireo/arquitecturadeapps/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.albireo.arquitecturadeapps
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+
+        }
     }
 }
